87c90e32b56e67a136d81de7dc532c1348bdb4b4
Esben Mortensen <esbenkam@gmail.com>
2019-09-12 13:05:58 +0200

    .

---
+++ CogSci Personallity test.R
@@ -0,0 +1,99 @@
+cogsci <- read.csv("data/NEW_CogSciPersonalityTest2019.csv", header = TRUE, sep=",")
+
+cogsci <- NEW_CogSciPersonalityTest2019
+
+
+#Her laver jeg fødselsdagen om til en dato som R så forstår
+cogsci$birth_day <- as.Date(cogsci$birth_day, format = c("%Y-%m-%d"))
+
+#Det er ikke alle som har skrevet den rigtige dage 
+mean(cogsci$birth_day, na.rm = TRUE)
+
+#Så jeg sortere alle dem fra, som har skrevet at deres fødselsdag er efter den 1. januar 2019
+cogsci <- cogsci %>% mutate(birth_day = replace(birth_day, birth_day > "2019-01-01", NA))
+
+mean(cogsci$birth_day, na.rm = TRUE)
+
+count(cogsci$birth_day > "2019-01-01")
+
+cogsci.query1 <- filter(cogsci, birth_day < "2019-01-01")
+summary(cogsci.query1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
